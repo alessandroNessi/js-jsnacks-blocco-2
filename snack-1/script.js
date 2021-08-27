978285fec74aff7f9d86b9f41cdb52795014b858
alessandroNessi <alessandronessi@msn.com>
2021-08-27 11:45:11 +0200

    added an exit keyword and a number of input

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,24 +1,31 @@
 var userInput;
 var sum=0;
 //for
-for(var i=0; i<5; i++){
-    userInput=parseInt(prompt("Insert the "+ (i+1) +" number:"));
-    if(isNaN(userInput)){
-        alert("you insered the wrong number! Try again");
-        i--;
-    }else{
-        sum +=userInput;
+var numberRequested=prompt("inserisci la quantità di numeri da inserire o \"exit\" per uscire durante l'inserimento");
+for(var i=0; i<numberRequested && userInput!="exit"; i++){
+    userInput=prompt("Insert the "+ (i+1) +" number:");
+    if(userInput!="exit"){
+        userInput=parseInt(userInput);
+        if(isNaN(userInput)){
+            alert("you insered the wrong number! Try again");
+            i--;
+        }else{
+            sum +=userInput;
+        }
     }
 }
 //while
 // var i=0;
-// while(i<5){
-//     userInput=parseInt(prompt("Insert the "+ (i+1) +" number:"));
-//     if(isNaN(userInput)){
-//         alert("you insered the wrong number! Try again");
-//     }else{
-//         sum +=userInput;
-//         i++;
-//     }
+// while(i<numberRequested && userInput!="exit"){
+    // userInput=prompt("Insert the "+ (i+1) +" number:");
+    // if(userInput!="exit"){
+    //     userInput=parseInt(userInput);
+    //     if(isNaN(userInput)){
+    //         alert("you insered the wrong number! Try again");
+    //     }else{
+    //         sum +=userInput;
+    //         i++;
+    //     }
+    // }     
 // }
 document.getElementById("risultato").innerHTML=sum;
