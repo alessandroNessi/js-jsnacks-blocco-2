56e12622cf35b0deddba9b340878588ef905783d
alessandroNessi <alessandronessi@msn.com>
2021-08-27 12:18:52 +0200

    esercizio completato

---
+++ snack-2/script.js
@@ -0,0 +1,19 @@
+//for
+var userInput=prompt("inserisci il numero: ");
+while(isNumber(userInput)==false){
+    userInput=prompt("Non hai inserito un numero, inserisci un numero: ");
+}
+userInput=parseInt(userInput);
+if(userInput%2==1){
+    userInput+=1;
+}
+document.getElementById("risultato").innerHTML=userInput;
+//function check if the input is onlynumbers
+function isNumber(temp){
+    for(let t=0; t<temp.length; t++){
+        if(temp[t]!="0"&&temp[t]!="1"&&temp[t]!="2"&&temp[t]!="3"&&temp[t]!="4"&&temp[t]!="5"&&temp[t]!="6"&&temp[t]!="7"&&temp[t]!="8"&&temp[t]!="9"){
+            return false;
+        }
+    }
+    return true;
+}
