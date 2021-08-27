7306b6d578c87e511ddd0af145aff19b80c803e2
alessandroNessi <alessandronessi@msn.com>
2021-08-27 12:11:57 +0200

    added while as samuele suggested

--- snack-1/script.js
+++ snack-1/script.js
@@ -9,13 +9,10 @@ numberRequested=parseInt(numberRequested);
 for(var i=0; i<numberRequested && userInput!="exit"; i++){
     userInput=prompt("Insert the "+ (i+1) +" number:");
     if(userInput!="exit"){
-        if(isNumber(userInput)){
-            userInput=parseInt(userInput);
-            sum +=userInput;
-        }else{
-            alert("you insered the wrong number! Try again");
-            i--;
+        while(isNumber(userInput)==false){
+            userInput=prompt("You insered the wrong number, reinsert the "+ (i+1) +" number:");
         }
+        sum +=userInput;
     }
 }
 //while
@@ -23,16 +20,15 @@ for(var i=0; i<numberRequested && userInput!="exit"; i++){
 // while(i<numberRequested && userInput!="exit"){
     // userInput=prompt("Insert the "+ (i+1) +" number:");
     // if(userInput!="exit"){
-    //     if(isNumber(userInput)){
-    //         userInput=parseInt(userInput);
-    //         sum +=userInput;
-    //         i++;
-    //     }else{
-    //         alert("you insered the wrong number! Try again");
-    //     }
+    // while(isNumber(userInput)==false){
+    //     userInput=prompt("You insered the wrong number, reinsert the "+ (i+1) +" number:");
+    // }
+    // i++;
+    // sum +=userInput;
     // }
 // }
 document.getElementById("risultato").innerHTML=sum;
+//function check if the input is onlynumbers
 function isNumber(temp){
     for(let t=0; t<temp.length; t++){
         if(temp[t]!="0"&&temp[t]!="1"&&temp[t]!="2"&&temp[t]!="3"&&temp[t]!="4"&&temp[t]!="5"&&temp[t]!="6"&&temp[t]!="7"&&temp[t]!="8"&&temp[t]!="9"){
