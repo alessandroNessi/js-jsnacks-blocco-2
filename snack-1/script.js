8928949f63a9c5df22356a8fbcad228a48b9598e
alessandroNessi <alessandronessi@msn.com>
2021-08-27 11:57:51 +0200

    fixed number bug on parseint

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,15 +2,19 @@ var userInput;
 var sum=0;
 //for
 var numberRequested=prompt("inserisci la quantità di numeri da inserire o \"exit\" per uscire durante l'inserimento");
+while(isNumber(numberRequested)==false){
+    numberRequested=prompt("Non hai inserito un numero corretto, inserisci la quantità di numeri da inserire o \"exit\" per uscire durante l'inserimento");
+}
+numberRequested=parseInt(numberRequested);
 for(var i=0; i<numberRequested && userInput!="exit"; i++){
     userInput=prompt("Insert the "+ (i+1) +" number:");
     if(userInput!="exit"){
-        userInput=parseInt(userInput);
-        if(isNaN(userInput)){
+        if(isNumber(userInput)){
+            userInput=parseInt(userInput);
+            sum +=userInput;
+        }else{
             alert("you insered the wrong number! Try again");
             i--;
-        }else{
-            sum +=userInput;
         }
     }
 }
@@ -29,3 +33,11 @@ for(var i=0; i<numberRequested && userInput!="exit"; i++){
     // }     
 // }
 document.getElementById("risultato").innerHTML=sum;
+function isNumber(temp){
+    for(let t=0; t<temp.length; t++){
+        if(temp[t]!="0"&&temp[t]!="1"&&temp[t]!="2"&&temp[t]!="3"&&temp[t]!="4"&&temp[t]!="5"&&temp[t]!="6"&&temp[t]!="7"&&temp[t]!="8"&&temp[t]!="9"){
+            return false;
+        }
+    }
+    return true;
+}
